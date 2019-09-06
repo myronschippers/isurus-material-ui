8091a85fc85292eda8e07be922e6b618ac7899db
Myron <dubtwee@gmail.com>
2019-09-06 13:56:05 -0500

    attempting to resolve an image pathing issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,10 @@ import PokemonCard from '../PokemonCard/PokemonCard';
 import Button from '@material-ui/core/Button';
 
 // MATERIAL
-import Grid from '@material-ui/core/Grid';
+import { 
+    Grid,
+    Container,
+} from '@material-ui/core';
 
 class App extends Component {
     render() {
@@ -22,29 +25,31 @@ class App extends Component {
                         <Button variant="contained" color="primary">Gotta Catch Them All</Button>
                     </div>
 
-                    <Grid container spacing={3}>
-                        <Grid item xs={3}>
-                            <PokemonCard />
-                        </Grid>
+                    <Container>
+                        <Grid container spacing={3}>
+                            <Grid item xs={3}>
+                                <PokemonCard />
+                            </Grid>
 
-                        <Grid item xs={3}>
-                            <img src="images/cubone.png" alt="Cubone" />
-                            <h3>Cubone</h3>
-                            <p>Etiam at elit ex. Nullam pellentesque dui nec varius congue. Fusce efficitur est eu libero vulputate, vel sollicitudin lorem rhoncus. Sed vestibulum a elit non ultricies. Etiam feugiat libero a fringilla faucibus. Fusce eu blandit tortor. Nunc gravida interdum sem, quis accumsan quam tincidunt interdum. Donec sed justo sit amet eros fringilla imperdiet hendrerit sit amet massa. Nunc non dapibus nibh. Nulla posuere enim ac est sollicitudin lobortis. Sed vel lectus efficitur.</p>
-                        </Grid>
+                            <Grid item xs={3}>
+                                <img src="images/cubone.png" alt="Cubone" />
+                                <h3>Cubone</h3>
+                                <p>Etiam at elit ex. Nullam pellentesque dui nec varius congue. Fusce efficitur est eu libero vulputate, vel sollicitudin lorem rhoncus. Sed vestibulum a elit non ultricies. Etiam feugiat libero a fringilla faucibus. Fusce eu blandit tortor. Nunc gravida interdum sem, quis accumsan quam tincidunt interdum. Donec sed justo sit amet eros fringilla imperdiet hendrerit sit amet massa. Nunc non dapibus nibh. Nulla posuere enim ac est sollicitudin lobortis. Sed vel lectus efficitur.</p>
+                            </Grid>
 
-                        <Grid item xs={3}>
-                            <img src="images/gible.png" alt="Gible" />
-                            <h3>Gible</h3>
-                            <p>Nullam pellentesque dui nec varius congue. Fusce efficitur est eu libero vulputate, vel sollicitudin lorem rhoncus. Sed vestibulum a elit non ultricies. Etiam feugiat libero a fringilla faucibus. Fusce eu blandit tortor. Nunc gravida interdum sem, quis accumsan quam tincidunt interdum. Donec sed justo sit amet eros fringilla imperdiet hendrerit sit amet massa. Nunc non dapibus nibh. Nulla posuere enim ac est sollicitudin lobortis.</p>
-                        </Grid>
+                            <Grid item xs={3}>
+                                <img src="images/gible.png" alt="Gible" />
+                                <h3>Gible</h3>
+                                <p>Nullam pellentesque dui nec varius congue. Fusce efficitur est eu libero vulputate, vel sollicitudin lorem rhoncus. Sed vestibulum a elit non ultricies. Etiam feugiat libero a fringilla faucibus. Fusce eu blandit tortor. Nunc gravida interdum sem, quis accumsan quam tincidunt interdum. Donec sed justo sit amet eros fringilla imperdiet hendrerit sit amet massa. Nunc non dapibus nibh. Nulla posuere enim ac est sollicitudin lobortis.</p>
+                            </Grid>
 
-                        <Grid item xs={3}>
-                            <img src="images/eevee.png" alt="Eevee" />
-                            <h3>Eevee</h3>
-                            <p>Fusce efficitur est eu libero vulputate, vel sollicitudin lorem rhoncus. Sed vestibulum a elit non ultricies. Etiam feugiat libero a fringilla faucibus. Fusce eu blandit tortor. Nunc gravida interdum sem, quis accumsan quam tincidunt interdum. Donec sed justo sit amet eros fringilla imperdiet hendrerit sit amet massa. Nunc non dapibus nibh. Nulla posuere enim ac est sollicitudin lobortis.</p>
+                            <Grid item xs={3}>
+                                <img src="images/eevee.png" alt="Eevee" />
+                                <h3>Eevee</h3>
+                                <p>Fusce efficitur est eu libero vulputate, vel sollicitudin lorem rhoncus. Sed vestibulum a elit non ultricies. Etiam feugiat libero a fringilla faucibus. Fusce eu blandit tortor. Nunc gravida interdum sem, quis accumsan quam tincidunt interdum. Donec sed justo sit amet eros fringilla imperdiet hendrerit sit amet massa. Nunc non dapibus nibh. Nulla posuere enim ac est sollicitudin lobortis.</p>
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </Container>
 
                 </main>
 
--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -7,16 +7,21 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 
+import './PokemonCard.css';
+
 class PokemonCard extends Component {
     render() {
         return (
             <Card>
                 <CardMedia
-                    img="images/pikachu.png"
+                    img="/static/images/pikachu.png"
                     title="Pikachu"
+                    className="pokeImg"
                 />
-                <h3>Pikachu</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at elit ex. Nullam pellentesque dui nec varius congue. Fusce efficitur est eu libero vulputate, vel sollicitudin lorem rhoncus. Sed vestibulum a elit non ultricies. Etiam feugiat libero a fringilla faucibus. Fusce eu blandit tortor. Nunc gravida interdum sem, quis accumsan quam tincidunt interdum. Donec sed justo sit amet eros fringilla imperdiet hendrerit sit amet massa. Nunc non dapibus nibh.</p>
+                <CardContent>
+                    <h3>Pikachu</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at elit ex. Nullam pellentesque dui nec varius congue. Fusce efficitur est eu libero vulputate, vel sollicitudin lorem rhoncus. Sed vestibulum a elit non ultricies. Etiam feugiat libero a fringilla faucibus. Fusce eu blandit tortor. Nunc gravida interdum sem, quis accumsan quam tincidunt interdum. Donec sed justo sit amet eros fringilla imperdiet hendrerit sit amet massa. Nunc non dapibus nibh.</p>
+                </CardContent>
             </Card>
         );
     }
